914a2015962de4364597e89abe142322b26b3caf
alielrafeiFCIH <alielrafei.fcih@gmail.com>
2019-03-21 14:05:40 +0200

    oder model added

--- repair/models.py
+++ repair/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class order(models.Model):
+    phone=models.CharField(max_length=50)
+    name=models.CharField(max_length=50)
+    payed=models.DecimalField(decimal_places=2,max_digits=1000)
+    reminder=models.DecimalField(decimal_places=2,max_digits=1000)
+    total_price=models.DecimalField(decimal_places=2,max_digits=1000)
+    current_date=models.DateField()
+    estimate_date=models.DateField()
+
