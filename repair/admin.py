67a69d27e59b2bab21478ffec9a7665a78f5bb85
alielrafeiFCIH <alielrafei.fcih@gmail.com>
2019-03-22 23:18:30 +0200

    done

--- repair/admin.py
+++ repair/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Repair_order,Repair_item,Repair_option
 # Register your models here.
-admin.site.register(Repair_order)
-admin.site.register(Repair_item)
-admin.site.register(Repair_option)
+admin.site.register(Repair_order,Repair_item,Repair_option)
 
