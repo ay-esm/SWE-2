38ff90a94db7a12b924c3a5ca9d06bfc6b140382
Ahmed Mohamadeen <2m.fcih@fci.helwan.edu.eg>
2019-03-22 21:11:45 +0200

    Added UI for new order for new customer  #1

--- customer/models.py
+++ customer/models.py
@@ -5,3 +5,5 @@ class Customer(models.Model):
     name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
 
+    def __str__(self):
+        return "id :" + str(self.id)+" name :" + self.name
