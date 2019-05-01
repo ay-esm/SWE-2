790b7edfbd6205625a647253424dd70aaed317bd
Ahmed Mohamadeen <2m.fcih@fci.helwan.edu.eg>
2019-05-01 22:17:48 +0200

    finished Add item and list item and pdf #3 #5

--- repair/forms.py
+++ repair/forms.py
@@ -3,6 +3,22 @@ from django import forms
 from .models import *
 from django.core.validators import RegexValidator
 from django.forms import formset_factory
+from django.forms import SelectDateWidget
+
+
+class Repair_order_From(forms.Form):
+
+    estimate_data = forms.DateField(widget=SelectDateWidget(),required=True, label='تاريخ التسليم')
+
+    class Meta:
+        model = Repair_order
+        fields = ['estimate_data']
+        widgets = {}
+
+# forms.DateInput(format='%Y/%m/%d', attrs={'class': 'js-datepicker form-control',
+#                                                               'id': 'example-datepicker1',
+#                                                               'data-date-format': 'yy/mm/dd',
+#                                                               'placeholder': 'تاريخ التسليم'})
 class Repair_item_From(forms.Form):
     type1_option = [
         ('-', '-'),
@@ -11,7 +27,7 @@ class Repair_item_From(forms.Form):
         ('اسورة عجلة', 'اسورة عجلة'),
         (' اسورة مفرودة', 'اسورة مفرودة'),
         ('بورتكلية','بورتكلية'),
-        ('تعليقة','تعليقةر'),
+        ('تعليقة','تعليقة'),
         ('توينز','توينز'),
         ('حرف','حرف'),
         ('حلق','حلق'),
@@ -83,8 +99,6 @@ class Repair_item_From(forms.Form):
         ('اخري','اخري'),
     ]
 
-    numeric = RegexValidator(r'^[1-9]*$', 'Only numeric characters are allowed.')
-
     type1 =forms.CharField(required=True, label="البيان", widget=forms.Select(choices=type1_option))
 
     type2 =forms.CharField(required=True, label="الصنف", widget=forms.Select(choices=type2_option))
@@ -93,12 +107,12 @@ class Repair_item_From(forms.Form):
 
     option = forms.CharField(required=True, widget=forms.Select(choices=repair_option), label='المطلوب')
 
-    price =forms.DecimalField(required=True, validators=[numeric], label="السعر", decimal_places=2,
-                              widget=forms.TextInput(attrs={
+    price =forms.DecimalField(required=True, label="السعر", decimal_places=2,
+                              widget=forms.NumberInput(attrs={
                                 'class': 'form-control ta-r',
                                 'placeholder': '0',
                             }))
-    summary = forms.CharField(widget=forms.Textarea,label='الملاحظات')
+    summary = forms.CharField(required=True, widget=forms.Textarea,label='الملاحظات')
 
     class Meta:
         model = Repair_item
@@ -106,4 +120,5 @@ class Repair_item_From(forms.Form):
         fields = ['type1', 'type2', 'type3', 'option', 'price', 'summary']
 
 
-repair_form_set = formset_factory(Repair_item_From, extra=0)
+repair_form_set = formset_factory(Repair_item_From, extra=0, can_delete=True)
+
--- repair/views.py
+++ repair/views.py
@@ -3,6 +3,7 @@ from .filters import *
 from customer.forms import CustomerForm, CustomerForm2
 from customer.models import Customer
 from repair.forms import *
+from repair.models import *
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from urllib.parse import urlencode
@@ -17,15 +18,20 @@ def add_order_view(request):
         if form.is_valid():
             phone = request.POST.get("phone")
             #print(phone)
-            cu = Customer.objects.filter(phone=phone).values('name')
+            cu = None
+            if Customer.objects.filter(phone=phone):
+                cu = Customer.objects.get(phone=phone)
             # print(cu[0]['name'])
             if cu:
-                name = cu[0]['name']
-                form = CustomerForm(initial={'name':name,'phone':phone})
-                context ={'form':form}
-                return redirect('addItem/'+'?'+'phone='+phone)
+                order_obj = Repair_order(customer_id=cu)
+                order_obj.save()
+                base_url = reverse('repair:AddI')  # 1 /addItem/
+                query_string = urlencode({'phone': request.POST['phone'], 'order': order_obj.id})  # 2 phone=011
+                url = '{}?{}'.format(base_url, query_string)  # concatenate to this form  /addItem/?phone=011&order=1
+                return redirect(url)
             else:
-                return redirect('add/', request)
+                url = reverse('repair:AddC')
+                return redirect(url, request)
 
     else:
         form = CustomerForm()
@@ -39,10 +45,12 @@ def add_customer_view(request):
     if request.method == "POST":
         new_customer = Customer(name=request.POST['name'], phone=request.POST['phone'])
         new_customer.save()
-        context = {'form': form}
+        order_obj = Repair_order(customer_id=new_customer)
+        order_obj.save()
         base_url = reverse('repair:AddI')  # 1 /addItem/
-        query_string = urlencode({'phone': request.POST['phone']})  # 2 phone=011
-        url = '{}?{}'.format(base_url, query_string)  # concatenate to this form  /addItem/?phone=011
+        query_string = urlencode({'phone': request.POST['phone'], 'order': order_obj.id})  # 2 phone=011
+        url = '{}?{}'.format(base_url, query_string)  # concatenate to this form  /addItem/?phone=011&order=1
+
         return redirect(url)
     else:
         context = {'form': form}
@@ -61,23 +69,53 @@ def add_items(request):
             name = cu[0]['name']
             form = CustomerForm(initial={'name': name, 'phone': phone})
             context = {'form': form}
+            Dataform = Repair_order_From()
+            context['date'] = Dataform
             if request.method == 'GET':
-                formset = repair_form_set(initial=[{'type1':'-','type2':'-','type3':'-','option':'-','price':0,'summary':''}])
+                print(context)
+                    # On first render print the formsets
+
+                formset = repair_form_set(initial=[{'type1': '-', 'type2': '-', 'type3': '-', 'option': '-', 'price': 0,
+                                                    'summary': ''}])
+
                 context['formset'] = formset
+
                 return render(request, 'repair/AddItem.html', context)
             elif request.method == 'POST':
-                formset = repair_form_set()
-                context['formset'] = formset
-            if formset.is_valid():
-                for form in formset:
-                    type1 = form.cleaned_data.get('type1')
-                    type2 = form.cleaned_data.get('type2')
-                    type3 = form.cleaned_data.get('type3')
-                    option = form.cleaned_data.get('option')
-                    summary = form.cleaned_data('summary')
-                    price = form.cleaned_data('price')
+                # submitted N form for N items
+                request_params = request.POST
+                formset = repair_form_set(request_params)
+                # print(type(formset[0]))  type: repair item form
+                #print("HI")
 
-                return redirect('repair/list_repairs.html')
+                if formset.is_valid():
+                    #print("HI")
+                    for form in formset:
+                        print("valid")
+                        if form.is_valid():
+                            # if form.cleaned_data.get('type1') and form.cleaned_data.get('type2') and form.cleaned_data.get('type3')  and form.cleaned_data.get('option') and form.cleaned_data('summary') and form.cleaned_data('price'):
+                            type1 = form.cleaned_data.get('type1')
+                            type2 = form.cleaned_data.get('type2')
+                            type3 = form.cleaned_data.get('type3')
+                            option = form.cleaned_data.get('option')
+
+                            summary = form.cleaned_data.get('summary')
+                            price = form.cleaned_data.get('price')
+                            order_pk = request.GET['order']
+                            order = Repair_order.objects.get(pk=order_pk)
+                            current_order_price = order.total_price
+                            order.total_price=current_order_price+price
+                            order.reminder=current_order_price+price
+                            order.save()
+                            item = Repair_item(type1=type1, type2=type2, type3=type3,repair_order_id=order,
+                                               summary=summary, price=price)
+                            item.save()
+                            option_i = Repair_option(option_name=option,)
+                            option_i.save()
+                            option_i.items_option.add(item)
+
+                url = reverse('repair:listrepairs')
+                return redirect(url)
         else:
             if request.method == 'GET':
                 formset = repair_form_set(initial=[{'type1': '-', 'type2': '-', 'type3': '-', 'option': '-', 'price': 0,
@@ -85,19 +123,20 @@ def add_items(request):
                 context['formset'] = formset
                 return render(request, 'repair/AddItem.html', context)
             elif request.method == 'POST':
-                formset = repair_form_set()
-                context['formset'] = formset
-            if formset.is_valid():
-                for form in formset:
-                    type1 = form.cleaned_data.get('type1')
-                    type2 = form.cleaned_data.get('type2')
-                    type3 = form.cleaned_data.get('type3')
-                    option = form.cleaned_data.get('option')
-                    summary = form.cleaned_data('summary')
-                    price = form.cleaned_data('price')
-                url = reverse('repair:listrepairs')
-                return redirect(url)
-    return render(request, 'repair/AddI4tem.html', context)
+                #print("hi")
+                formset = repair_form_set(request.POST)
+                if formset.is_valid():
+                    #print("hi")
+                    for form in formset:
+                        type1 = form.cleaned_data.get('type1')
+                        type2 = form.cleaned_data.get('type2')
+                        type3 = form.cleaned_data.get('type3')
+                        option = form.cleaned_data.get('option')
+                        summary = form.cleaned_data('summary')
+                        price = form.cleaned_data('price')
+                    url = reverse('repair:listrepairs')
+                    return redirect(url)
+    return render(request, 'repair/AddItem.html', context)
 
 
 @login_required
@@ -106,8 +145,6 @@ def list_repairs(request):
     order_filter = repair_filter(request.GET, queryset=order_list)
     context={'filter':order_filter}
     return render(request, 'repair/list_repairs.html', context)
-
-
 
 
 # def create_book_normal(request):
@@ -130,4 +167,5 @@ def list_repairs(request):
 #         'formset': formset,
 #         'heading': heading_message,
 # })
+
 #https://medium.com/@taranjeet/adding-forms-dynamically-to-a-django-formset-375f1090c2b0?fbclid=IwAR239lUIbWG7suikvZrNlyp_T71xKDy_t1ThVCyE_YzaF6xeCHIEBQNgkvQ
